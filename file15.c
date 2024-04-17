c7a2357f0660185552c9f4c4f555941804cc8482
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2024-04-17 15:37:16 +0300

    declaring , defining and calling funtions

--- file15.c
+++ file15.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int n, result = 1;
+    int n;
+    int factorial(int);
 
     printf("Enter a number: ");
     scanf("%d", &n);
+    
+    printf("The factorial of %d is %d\n", n,factorial(n));
+
+
+}
+
+int factorial(int n) {
+    int result = 1;
 
     if (n > 0) {
         while (n > 1) {
             result *= n;
             n--;
         }
-        printf("The factorial of %d is %d\n", n, result);
-    } else {
-        printf("Invalid input. Please enter a positive integer.\n");
     }
 
-    return 0;
+
+    return result;
 }
